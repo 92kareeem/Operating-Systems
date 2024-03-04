8aa9749b5d33221cb841a563700dd9ec370e87e1
Syed Abdul Kareem Ahmed <160421748048@mjcollege.ac.in>
2024-03-04 22:03:00 +0530

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,47 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-    int i,n,ts,count=0,t,twt,ttat;
+int main() {
+    int i, n, ts, count = 0, t = 0, twt = 0, ttat = 0;
     printf("Enter number of Processes: ");
-    scanf("%d",&n);
-    int BT[n],RT[n],WT[n],TAT[n];
+    scanf("%d", &n);
+    int BT[n], RT[n], WT[n], TAT[n], AT[n];
     printf("\nEnter Time Slice: ");
-    scanf("%d",&ts);
-    for(i=1;i<=n;i++){
-        printf("\nEnter BT of Process %d: ",i+1);
-        scanf("%d",&BT[i]);
-    }
-    for(i=1;i<=n;i++){
+    scanf("%d", &ts);
+    for (i = 0; i < n; i++) {
+        printf("\nEnter AT and BT of Process %d: ", i + 1);
+        scanf("%d %d", &AT[i], &BT[i]);
         RT[i] = BT[i];
     }
-    while(1){
-        for(i=1;i<=n;i++){
-            if(RT[i] > 0){
-                if(RT[i] > ts){
-                    t = t + ts;
-                    RT[i] = RT[i] - ts;
-                }
-            else if(RT[i] <= ts){
-                    t = t + RT[i];
+    while (1) {
+        int flag = 0; 
+        for (i = 0; i < n; i++) {
+            if (RT[i] > 0 && AT[i] <= t) {
+                flag = 1; 
+                if (RT[i] > ts) {
+                    t += ts;
+                    RT[i] -= ts;
+                } else {
+                    t += RT[i];
                     RT[i] = 0;
-                    TAT[i] = t;
-                    WT[i] = t - BT[i];
+                    TAT[i] = t - AT[i];
+                    WT[i] = TAT[i] - BT[i];
                     count++;
                 }
             }
         }
-        if(count==n)
-        break;
+        if (flag == 0)
+            t++;
+        if (count == n) 
+            break;
     }
-    for(i=1;i<=n;i++){
-        twt = twt + WT[i];
-        ttat = ttat + TAT[i];
+    for (i = 0; i < n; i++) {
+        twt += WT[i];
+        ttat += TAT[i];
     }
-    printf("\nProcess\tBT\tWT\tTAT\n");
-    for(i=1;i<=n;i++){
-        printf("\nP%d\t%d\t%d\t%d\n",i,BT[i],WT[i],TAT[i]);
+    printf("\nProcess\tAT\tBT\tWT\tTAT\n");
+    for (i = 0; i < n; i++) {
+        printf("\nP%d\t%d\t%d\t%d\t%d\n", i + 1, AT[i], BT[i], WT[i], TAT[i]);
     }
-    float awt = (float)twt/n;
-    float attat = (float)ttat/n;
-    printf("\nAWT: %1.2f ATAT: %1.2f",awt,attat);
+    float awt = (float)twt / n;
+    float attat = (float)ttat / n;
+    printf("\nAWT: %1.2f ATAT: %1.2f", awt, attat);
+    return 0;
 }
